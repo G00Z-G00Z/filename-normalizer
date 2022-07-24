3b546dba3beb584c66624183811ad1cce48c2b3f
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-24 11:42:35 -0500

    Changed the docs for the root command

--- cmd/root.go
+++ cmd/root.go
@@ -10,21 +10,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "filename-normalizer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A cli-app that normalizes the name of files",
+	Long:  `A cli-app that normalizes the name of files with several different techniques`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +35,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
