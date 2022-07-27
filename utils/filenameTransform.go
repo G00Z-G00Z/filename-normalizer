30a7dfb01dc7f7ab0503ffb1a5ef4100ed7e6991
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-27 16:10:17 -0500

    Added function snake_case to cammelCase

--- utils/filenameTransform.go
+++ utils/filenameTransform.go
@@ -6,14 +6,22 @@ import (
 )
 
 var changeSpacesFor_Regex = regexp.MustCompile(`(?m) `)
+var snakeCase2CammelCaseRegex = regexp.MustCompile(`(?m)_(\w)`)
 
-const substitution = "_"
+const subSpaces = "_"
 
 // Transforms spaced out names to snake case
-func changeSpacesFor_(name string) string {
-	return changeSpacesFor_Regex.ReplaceAllString(name, substitution)
+func changeSpacesFor_(s string) string {
+	return changeSpacesFor_Regex.ReplaceAllString(s, subSpaces)
 }
 
 func Spaces2SnakeCase(s string) string {
 	return strings.ToLower(changeSpacesFor_(s))
+}
+
+func SnakeCase2CammelCase(s string) string {
+	return snakeCase2CammelCaseRegex.ReplaceAllStringFunc(s, func(m string) string {
+		return strings.ToUpper(strings.Trim(m, "_"))
+	})
+
 }
--- utils/filenameTransform_test.go
+++ utils/filenameTransform_test.go
@@ -1,37 +1,94 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 
-var tests []TestUnit[string, string]
+var (
+	testForRemovingSpaces    []TestUnit[string, string]
+	testToSnakeCase          []TestUnit[string, string]
+	testSnakeCase2CammelCase []TestUnit[string, string]
+)
 
 func init() {
 
-	tests = []TestUnit[string, string]{
+	testForRemovingSpaces = []TestUnit[string, string]{
 		{
-			Input:    "hey how are you.txt",
-			Expected: "hey_how_are_you.txt",
+			Input:    "hey how are you",
+			Expected: "hey_how_are_you",
 		}, {
-			Input:    "I Dont LIke Files wiTh spaces.txt",
-			Expected: "I_Dont_LIke_Files_wiTh_spaces.txt",
+			Input:    "I Dont LIke Files wiTh spaces",
+			Expected: "I_Dont_LIke_Files_wiTh_spaces",
 		}, {
-			Input:    "heLLoYou.EXE",
-			Expected: "heLLoYou.EXE",
+			Input:    "heLLoYou",
+			Expected: "heLLoYou",
 		},
 		{
-			Input:    "hello.txt",
-			Expected: "hello.txt",
+			Input:    "hello",
+			Expected: "hello",
+		},
+	}
+
+	testToSnakeCase = []TestUnit[string, string]{
+		{
+			Input:    "hey how are you",
+			Expected: "hey_how_are_you",
+		}, {
+			Input:    "I Dont LIke Files wiTh spaces",
+			Expected: "i_dont_like_files_with_spaces",
+		}, {
+			Input:    "heLLoYou",
+			Expected: "helloyou",
+		},
+		{
+			Input:    "hello",
+			Expected: "hello",
+		},
+	}
+
+	testSnakeCase2CammelCase = []TestUnit[string, string]{
+		{
+			Input:    "hello",
+			Expected: "hello",
+		}, {
+			Input:    "i_am_not_a_king",
+			Expected: "iAmNotAKing",
+		}, {
+			Input:    "just_do_it",
+			Expected: "justDoIt",
+		}, {
+			Input:    "hello_how_are_you",
+			Expected: "helloHowAreYou",
 		},
 	}
 
 }
 
 func TestChangeSpacesFor_(t *testing.T) {
-	for _, test := range tests {
+	for _, test := range testForRemovingSpaces {
 		test.SetOutput(changeSpacesFor_(test.Input))
 
 		if !test.IsCorrect() {
 			test.DisplayError(t)
 		}
+	}
+}
 
+func TestSpaces2SnakeCase(t *testing.T) {
+	for _, test := range testToSnakeCase {
+		test.SetOutput(Spaces2SnakeCase(test.Input))
+		if !test.IsCorrect() {
+			test.DisplayError(t)
+		}
+	}
+}
+
+func TestSnakeCase2CammelCase(t *testing.T) {
+	for _, test := range testSnakeCase2CammelCase {
+		test.SetOutput(SnakeCase2CammelCase(test.Input))
+
+		if !test.IsCorrect() {
+			test.DisplayError(t)
+		}
 	}
 }
