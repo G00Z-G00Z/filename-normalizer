84da6f32419effd5fe9b26f6f1dcbb3ebc226328
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-27 15:51:52 -0500

    Made function to transform to snake case

--- utils/filenameTransform.go
+++ utils/filenameTransform.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"regexp"
+	"strings"
 )
 
 var changeSpacesFor_Regex = regexp.MustCompile(`(?m) `)
@@ -11,4 +12,8 @@ const substitution = "_"
 // Transforms spaced out names to snake case
 func changeSpacesFor_(name string) string {
 	return changeSpacesFor_Regex.ReplaceAllString(name, substitution)
+}
+
+func Spaces2SnakeCase(s string) string {
+	return strings.ToLower(changeSpacesFor_(s))
 }
