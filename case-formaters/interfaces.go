11436719fcd10d6b79a9a036b346d8162600cb38
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-29 11:34:37 -0500

    Added string Formatter type

--- case-formaters/interfaces.go
+++ case-formaters/interfaces.go
@@ -3,5 +3,8 @@ package caseformaters
 // Function that takes a string and returns a modified string
 type CaseFormatter func(s string, currentFormat CaseFormat) string
 
+// Function that manipulates a string
+type StringFormatter func(s string) string
+
 // Function that takes a CaseFormatter and decorates it
 type CaseFormatterDecorator func(f CaseFormatter) CaseFormatter
