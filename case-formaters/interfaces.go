0333b749a15afb9a492ab6446e5b8dc7c5c878bd
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-29 10:44:56 -0500

    Added a current format variable to interface

--- case-formaters/interfaces.go
+++ case-formaters/interfaces.go
@@ -1,7 +1,7 @@
 package caseformaters
 
 // Function that takes a string and returns a modified string
-type CaseFormatter func(string) string
+type CaseFormatter func(string, currentFormat CaseFormat) string
 
 // Function that takes a CaseFormatter and decorates it
 type CaseFormatterDecorator func(CaseFormatter) CaseFormatter
