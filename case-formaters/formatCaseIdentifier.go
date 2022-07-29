643471d3b3e3b285afc0cb63fa8efda029ea4549
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-29 12:56:45 -0500

    Made the case identifier more general

--- case-formaters/formatCaseIdentifier.go
+++ case-formaters/formatCaseIdentifier.go
@@ -2,19 +2,17 @@ package caseformaters
 
 import "strings"
 
+// Identifies the case format based on the data map
 func IdentifyCaseFormat(s string) CaseFormat {
 
-	// Has spaces
-	if strings.Contains(s, " ") {
-		return SpacesCase
+	for format, data := range caseFormattersMetaData {
+
+		if data.IsCase(s) {
+			return format
+		}
 	}
 
-	// Has underscores with no spaces
-	if strings.Contains(s, "_") {
-		return SnakeCase
-	}
-
-	return CammelCase
+	return Unkwon
 }
 
 //  Identifiers
